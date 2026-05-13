4803e4e0e4c10bee2e06cd14a382acc261b5d182
Monika Shakya <monikashakyaofficial@gmail.com>
2026-05-13 13:25:39 +1000

    Fix next config ES module syntax

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,27 +1,21 @@
 import { Inter, Playfair } from "next/font/google";
 import "./globals.css";
-import Navbar from "../components/navbar";
 const inter = Inter({ subsets: ["latin"] });
-const playfair = Playfair({ subsets: ["latin"], variable: '--font-Play-mono', });
-
+const playfair = Playfair({ subsets: ["latin"], variable: "--font-Play-mono" });
 
 export const metadata = {
   title: "Monika Shakya",
   description: "A Computer Engineer, Web and Data enthusiat",
-  icons: [{ rel: 'icon', url: '/public/favicon.ico' }],
+  icons: [{ rel: "icon", url: "/public/favicon.ico" }],
 };
 
-export default function RootLayout({
-  children,
-}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel='icon' href='/favicon.ico' />
-
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body className={`${inter.className} ${playfair.variable}`}>
-        <Navbar />
         {children}
       </body>
     </html>
