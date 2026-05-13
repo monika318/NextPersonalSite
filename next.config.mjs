ba1b629f1af740075eb6521369b050108b4da8ae
Monika Shakya <monikashakyaofficial@gmail.com>
2026-05-13 13:21:40 +1000

    Clean URLs via Next.js rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/monika-web.html",
+      },
+      {
+        source: "/data",
+        destination: "/monika-data.html",
+      },
+    ];
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
